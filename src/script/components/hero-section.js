73b95c97f1de8de5e23341dff644c9302697641e
wraleydi <raleydi06@gmail.com>
2024-10-23 21:26:19 +0700

    styling section bidang

--- src/script/components/hero-section.js
+++ src/script/components/hero-section.js
@@ -46,8 +46,8 @@ class HeroSection extends HTMLElement {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Overlay hitam dengan transparansi */
-  z-index: 1; /* Pastikan overlay ada di atas gambar */
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
 }
 
 .gallery-image {
